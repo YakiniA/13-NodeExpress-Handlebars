9ea06b04576452d86b4a8a45cd5dfbbca10f5357
Yakini <yakinilatha09@gmail.com>
2020-08-17 10:17:16 -0600

    Database set up

---
+++ Instructions/db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veg Burger', FALSE);
