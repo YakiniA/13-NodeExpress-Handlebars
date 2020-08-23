a7daa983bf0b5b7f5373f61be9470c8163873ff2
Yakini <yakinilatha09@gmail.com>
2020-08-23 16:28:55 -0600

    problem displaying the errormasg on no input

--- config/orm.js
+++ config/orm.js
@@ -1,21 +1,6 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
-// Helper function for SQL syntax.
-// Let's say we want to pass 3 values into the mySQL query.
-// In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
-// ["?", "?", "?"].toString() => "?,?,?";
-function printQuestionMarks(num) {
-  var arr = [];
-
-  for (var i = 0; i < num; i++) {
-    arr.push("?");
-  }
-
-  return arr.toString();
-}
-
 // Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
   var arr = [];
@@ -52,18 +37,32 @@ var orm = {
   },
    create: function(table, cols, vals, cb) {
      var boolean = false;
-     var queryString = `INSERT INTO ${table} (${cols}) VALUES ('${vals}', ${boolean});`
+    console.log("Vals Name" +vals);
+    console.log("Type of Vals Name" +typeof vals)
 
-     console.log(queryString);
+      if(vals != ""){
+      var queryString = `INSERT INTO ${table} (${cols}) VALUES ('${vals}', ${boolean});`
+      console.log(queryString);
+      connection.query(queryString, vals, function(err, result) {
+        if (err) {
+          throw err;
+        }
+ 
+        cb(result);
+      });
+      }else{
+         alert("Please enter the burger name");
 
-     connection.query(queryString, vals, function(err, result) {
-       if (err) {
-         throw err;
+           $(document).ready(function () {
+            var errmsg = $("<div>").addClass("errMsg").text("Please enter the burger name");
+             $("#ca").append(errmsg);
+          });
        }
+   
 
-       cb(result);
-     });
+      }
    },
+
    //An example of objColVals would be {name: panther, sleepy: true}
    update: function(table, objColVals, condition, cb) {
 
